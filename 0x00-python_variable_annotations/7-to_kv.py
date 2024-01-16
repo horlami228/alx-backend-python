991cf942be86aa44689ffffed9af7637c869d245
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-16 14:44:49 +0100

    submited

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""returning a tuple"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple of (key, value) from input arguments."""
+    return k, v ** 2
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""make multiplier"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies its argument
+    by the given multiplier."""
+    def multiplier_func(value: float) -> float:
+        return value * multiplier
+
+    return multiplier_func
