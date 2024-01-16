375e38f59b212acea9f1f697bf1478bd0cffe8d1
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-16 14:07:27 +0100

    python anot

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of all numbers in a list."""
+    return sum(input_list)
