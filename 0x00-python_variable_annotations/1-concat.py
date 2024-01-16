98b935e465c7dd3777b163fb303a6d5fc23fda96
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-16 13:08:43 +0100

    python annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    type-annotated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function takes in two string
+    and returns a concatenated string
+    """
+
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """return the floor
+
+    """
+    return math.floor(n)
