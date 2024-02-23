58579b4beeb1d255802a5a82c0edd9125fc78ea4
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-23 11:15:14 +0100

    Refactor test_org method in TestGithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,18 +8,28 @@ from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class for Testing Github Org Client """
-
+    """
+    TestGithubOrgClient class
+    """
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google",),
+        ("abc",),
     ])
     @unittest.mock.patch('client.get_json')
-    def test_org(self, input, mock):
-        """Test that GithubOrgClient.org returns the correct value"""
-        test_class = GithubOrgClient(input)
-        test_class.org()
-        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+    def test_org(self, org_name, mock_object):
+        """test org method"""
+        url = f"https://api.github.com/orgs/{org_name}"
+        expected_result = {"name": org_name, "payload": True}
+
+        mock_object.return_value = expected_result
+
+        client = GithubOrgClient(org_name)
+
+        result = client.org
+
+        mock_object.assert_called_once_with(url)
+
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == "__main__":
